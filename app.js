549ae6e19995854b90026baa97b9b9041e1da48a
yudhies2109 <ma.yudhistira@hotmail.com>
2023-09-25 10:25:46 +0700

    connect to db

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const { Pool, Client } = require('pg');
+
+const pool = new Pool ({
+  user : 'yudhistira',
+  database : 'sds_system',
+  password : 'Mydatabase',
+  host : 'localhost',
+  port : 5432
+});
+
+// Test Conect to Db
+
+pool.connect( err => {
+  if(err){
+    console.log(err.message)
+  } else {
+    console.log('connected')
+  }
+})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
