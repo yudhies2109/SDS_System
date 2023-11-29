2673ddeff160ea5c770f7dcb2750a8ee794f7e35
yudhies2109 <ma.yudhistira@hotmail.com>
2023-11-29 13:06:52 +0700

    added style navbar home page

--- public/javascripts/expand.js
+++ public/javascripts/expand.js
@@ -48,3 +48,18 @@ function rotateImg() {
         colortext.style.color = '#070B0F';
     }
 }
+
+// FUNCTION CROUSEL ABOUT
+
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+    list.scrollLeft -= (itemWidth + padding)
+})
+next.addEventListener('click',()=>{
+    list.scrollLeft += (itemWidth + padding)
+})
